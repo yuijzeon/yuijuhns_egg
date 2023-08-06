b778d363711e05a8ac35f8f64afe56e81b3feefe
Tintin Ting <dingyujen@gmail.com>
2023-08-07 00:45:09 +0800

    feat: post crawling data to gcp logging

--- app/src/main/java/com/yuijuhn/egg/CrawlingService.kt
+++ app/src/main/java/com/yuijuhn/egg/CrawlingService.kt
@@ -8,33 +8,36 @@ import android.util.Log
 import android.view.Display
 import android.view.accessibility.AccessibilityEvent
 import android.widget.Toast
+import com.google.gson.Gson
 import com.googlecode.tesseract.android.TessBaseAPI
 import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 
 
 class CrawlingService : AccessibilityService() {
     private val handler = Handler(Looper.getMainLooper())
     private lateinit var tess: TessBaseAPI
 
-    @OptIn(DelicateCoroutinesApi::class)
     private val runnable = Runnable {
-        GlobalScope.launch {
-            try {
-                main()
-            } finally {
-                continueCrawling()
-            }
+        try {
+            main()
+        } finally {
+            continueCrawling()
         }
     }
 
     override fun onServiceConnected() {
         super.onServiceConnected()
 
-        Assets.extractAssets(this);
+        Assets.extractAssets(this)
         tess = TessBaseAPI()
-        if (!tess.init(filesDir.absolutePath, "eng")) {
+        if (!tess.init(filesDir.absolutePath, "chi_tra")) {
             onDestroy()
             return
         }
@@ -43,30 +46,50 @@ class CrawlingService : AccessibilityService() {
         continueCrawling()
     }
 
-    private suspend fun main() {
+    private fun main() {
         captureScreenshot()
 
-        handler.post {
-            Toast.makeText(this, "Crawling", Toast.LENGTH_SHORT).show()
-        }
+//        handler.post {
+//            Toast.makeText(this, "Crawling", Toast.LENGTH_SHORT).show()
+//        }
     }
 
     private fun continueCrawling() {
         handler.postDelayed(runnable, 10000L)
     }
 
+    @OptIn(DelicateCoroutinesApi::class)
     private fun captureScreenshot() {
         val callback = object : TakeScreenshotCallback {
             override fun onSuccess(result: ScreenshotResult) {
                 val originalBitmap = Bitmap.wrapHardwareBuffer(result.hardwareBuffer, result.colorSpace)
                 val argb8888Bitmap = originalBitmap?.copy(Bitmap.Config.ARGB_8888, true)
 
+                if (argb8888Bitmap == null) {
+                    Log.d("TINTIN", "argb8888Bitmap is null")
+                    return
+                }
+
                 tess.setImage(argb8888Bitmap)
-                val text = tess.getHOCRText(0)
+                val text = tess.getHOCRText(1)
                 Log.d("TINTIN", text)
 
                 handler.post {
-                    Toast.makeText(applicationContext, "Got Screenshot", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(applicationContext, tess.utF8Text, Toast.LENGTH_SHORT).show()
+                }
+
+                val json = object {
+                    val pages: MutableList<String> = mutableListOf(text)
+                }
+                val body = Gson().toJson(json).toRequestBody("application/json".toMediaType())
+
+                val request = Request.Builder()
+                    .url("https://script.google.com/macros/s/AKfycbyAMg7YzDSMWGk0cAePOYEIX4aZsfj_m6W5xPQdZXDdye0l0XsGIaT-cCsPGHIUqT6x/exec")
+                    .post(body)
+
+                GlobalScope.launch(Dispatchers.IO) {
+                    val response = OkHttpClient().newCall(request.build()).execute()
+                    Log.d("TINTIN", "Response: " + response.body?.string())
                 }
             }
 
